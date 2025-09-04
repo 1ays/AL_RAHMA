963487535aa4b5cbcba53cff83438afc80813c15
aya <ayaayaaya1741@gmail.com>
2025-09-05 00:22:06 +0200

    the first version

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+ const app = express();
+ const port = 3000;
+
+ app.get("/",(req,res)=>{
+    res.render("home_page.ejs");
+ });
+
+ app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
